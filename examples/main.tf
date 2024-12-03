d2ce57087a4910489a26a284fe05f667719fe97b
osnatashush <osnat@delivops.com>
2024-12-03 18:57:15 +0200

    add an example

---
+++ examples/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_sns_topic" "sns_topic" {
+  name         = "sns"
+  display_name = "sns"
+}
+
+module "sqs_alerts" {
+  source = "../"
+
+
+
+
+  aws_sns_topics_arns = ["arn:aws:sns:eu-west-1:123456789012:sns"]
+  queue_name          = "sqs-1213"
+}
